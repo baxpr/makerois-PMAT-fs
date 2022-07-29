45e4d36aefb99dc1cb5050b3126b69194d505a9b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-07-28 21:11:36 -0400

    final

--- test_singularity.sh
+++ test_singularity.sh
@@ -6,9 +6,8 @@ singularity run \
 --bind INPUTS:/INPUTS \
 --bind OUTPUTS:/OUTPUTS \
 --bind freesurfer_license.txt:/usr/local/freesurfer/license.txt \
-makerois-PMAT_v1.0.13.simg \
-tseg_niigz /INPUTS/tseg.nii.gz \
-subj_dir /INPUTS/SUBJECT \
+makerois-PMAT-fs_v1.0.0-beta1.sif \
+aparc_mgz /INPUTS/aparc.a2009s+aseg.mgz \
 deffwd_niigz /INPUTS/y_t1.nii.gz \
 wt1_niigz /INPUTS/wmt1.nii.gz \
 mnigeom_nii avg152T1.nii \
